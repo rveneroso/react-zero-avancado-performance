40893baf1acbfb4fbb530040738080b01337e5b1
Renato Veneroso <rveneroso@gmail.com>
2024-11-22 19:56:38 -0300

    Código da aula 129 - Usando react-hook-form para controlar a entrada de dados em formulários.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState, useRef } from 'react'
+import { useRef } from 'react'
 import './App.css'
 import { Header } from './Header';
 
+import { useForm } from 'react-hook-form';
+
 
 function App() {
-  const nameRef = useRef(null);
-  const emailRef = useRef(null);
-  const userNameRef = useRef(null);
-  const descriptionRef = useRef(null)
 
-  function handleSave(e){
-    e.preventDefault();
+  const { register, handleSubmit } = useForm();
 
-    console.log({
-      name: nameRef.current?.value,
-      email: emailRef.current?.value,
-      username: userNameRef.current?.value,
-      description: descriptionRef.current?.value,
-    })
+  function handleSave(data){
+    console.log(data);
+
   }
 
 
@@ -26,34 +20,30 @@ function App() {
       <h1>React</h1>
       <Header/>
 
-      <form className="form" onSubmit={handleSave}>
+      <form className="form" onSubmit={handleSubmit(handleSave)}>
         <input
           type="text"
           placeholder="Digite seu nome..."
           className="input"
-          ref={nameRef}
+          {...register("name", { required: true })}
+          id="name"
         />
 
         <input
           type="text"
           placeholder="Digite seu email..."
           className="input"
-          ref={emailRef}
+          {...register("email", { required: true })}
+          id="email"
         />
 
         <input
           type="text"
           placeholder="Digite seu username..."
           className="input"
-          ref={userNameRef}
+          {...register("username", { required: true })}
+          id="username"
         />
-
-        <textarea
-          type="text"
-          placeholder="Digite sua descriçao..."
-          className="input"
-          ref={descriptionRef}
-        ></textarea>
 
         <button className="button" type="submit">Enviar</button>
       </form>
